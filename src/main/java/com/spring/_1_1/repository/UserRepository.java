026997a65441ca5bd139ff7369c04ee5849cd2f6
Тицкий Александр Александрович <titskyaa@mpei.ru>
2022-07-28 12:03:36 +0300

    second commit

--- src/main/java/com/spring/_1_1/repository/UserRepository.java
+++ src/main/java/com/spring/_1_1/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.spring._1_1.repository;
 
 import com.spring._1_1.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
 }
--- src/main/java/com/spring/_1_1/service/UserServiceImpl.java
+++ src/main/java/com/spring/_1_1/service/UserServiceImpl.java
@@ -3,7 +3,9 @@ package com.spring._1_1.service;
 import com.spring._1_1.model.User;
 import com.spring._1_1.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,15 +24,19 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<User> listUsers() {
-        return userRepository.findAll();
+        List<User> userList = new ArrayList<>();
+        userRepository.findAll().forEach(userList::add);
+        return userList;
     }
 
     @Override
+    @Transactional
     public void add(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
